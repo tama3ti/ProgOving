bd9aeca6427996bbae94f33e5e183f14b8ea1a5f
tama3ti <m.picheta@icloud.com>
2021-03-04 12:41:04 +0100

    finished person class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,7 +1,43 @@
-public class Person {
+public abstract class Person {
     private String firstName;
     private String lastName;
     private String socialSecurityNumber;
 
-    
+    public Person(String firstName, String lastName, String socialSecurityNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return getFirstName() + " " + getLastName();
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String toString() {
+        return getFullName() + " " + socialSecurityNumber;
+    }
 }
